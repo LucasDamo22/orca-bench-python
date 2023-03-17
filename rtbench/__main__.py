80464185958f761b0492afb3e09d4c914f2a2fc9
Lucas Damo <lucasmottadamo@gmail.com>
2023-03-17 16:07:29 -0300

    first commit

---
+++ rtbench/__main__.py
@@ -0,0 +1,80 @@
+"""
+This file is part of project ORCA. More information on the project
+can be found at the following repositories at GitHub's website.
+
+http://https://github.com/andersondomingues/orca-sim
+http://https://github.com/andersondomingues/orca-software
+http://https://github.com/andersondomingues/orca-mpsoc
+http://https://github.com/andersondomingues/orca-tools
+http://https://github.com/andersondomingues/orca-modeling
+
+Copyright (C) 2018-2020 Anderson Domingues, <ti.andersondomingues@gmail.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
+from __future__ import annotations
+import sys
+
+from rtbench.modeling.graph import Graph
+from rtbench.io.graph_file_handler import parse_graph_from_file
+from rtbench.io.terminal import info
+from rtbench.simulation.single_core_engine import SingleCoreEngine
+from rtbench.scheduling.deadline_monotonic import DeadlineMonotonic
+from rtbench.scheduling.earliest_deadline_first import EarliestDeadlineFirst
+from rtbench.scheduling.least_laxity_first import LeastLaxityFirst
+from rtbench.scheduling.least_slack_time import LeastSlackTime
+from rtbench.scheduling.rate_monotonic import RateMonotonic
+
+
+def main(args: list[str]):
+    # make sure we have all the arguments
+    if len(args) != 3:
+        print("Usage: python3 -m rtbench <TICKS> <APPGRAPH> <ALGORITHM>")
+        print(
+            "Produces a simulation trace of TICKS length for the given APPGRAPH, "
+            "applying the ALGORITHM scheduling."
+        )
+        exit(0)
+
+    ticks: int = int(args[0])
+    graph_file: str = args[1]
+    algorithm: str = args[2]
+
+    info(f"** Ticks: {ticks}")
+    info(f"** Graph: {graph_file}")
+    info(f"** Algorithm: {algorithm}")
+
+    graph: Graph = parse_graph_from_file(graph_file)
+
+    info(f"** Parsed data: {graph}")
+
+    algorithms = {
+        "EDF": EarliestDeadlineFirst,
+        "RM": RateMonotonic,
+        "DM": DeadlineMonotonic,
+        "LST": LeastSlackTime,
+        "LLF": LeastLaxityFirst,
+    }
+
+    alg_class = algorithms[algorithm]
+    print('aaaaaa')
+    engine = SingleCoreEngine(graph, alg_class)
+    engine.simulate(ticks)
+
+
+if __name__ == "__main__":
+    argv: list[str] = sys.argv[1:]
+    main(argv)
---
+++ rtbench/io/graph_file_handler.py
@@ -0,0 +1,130 @@
+"""
+This file is part of project ORCA. More information on the project
+can be found at the following repositories at GitHub's website.
+
+http://https://github.com/andersondomingues/orca-sim
+http://https://github.com/andersondomingues/orca-software
+http://https://github.com/andersondomingues/orca-mpsoc
+http://https://github.com/andersondomingues/orca-tools
+http://https://github.com/andersondomingues/orca-modeling
+
+Copyright (C) 2018-2020 Anderson Domingues, <ti.andersondomingues@gmail.com>
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+from enum import Enum
+from rtbench.modeling.graph import Graph, GraphEdge, GraphNode
+
+
+class GraphParseState(Enum):
+    IGNORE = 0
+    FIND_NODE_HEADER = 1
+    PARSE_NODES = 2
+    FIND_EDGE_HEADER = 3
+    PARSE_EDGES = 4
+
+
+def parse_graph_from_file(file_name: str) -> Graph:
+    NODE_HEADER = "[nodes]"
+    EDGE_HEADER = "[edges]"
+
+    graph = Graph()
+   
+    with open(file_name) as file:
+        state: GraphParseState = GraphParseState.IGNORE
+
+        line: str
+
+        while True:
+            line = file.readline()
+            
+            if not line:
+                break
+
+            if line[0] == "#" or line[0] == "\n" or line == "":
+                continue
+
+            if line.startswith(NODE_HEADER):
+                state = GraphParseState.PARSE_NODES
+                continue
+            elif line.startswith(EDGE_HEADER):
+                state = GraphParseState.PARSE_EDGES
+                continue
+
+            if state in [
+                GraphParseState.IGNORE,
+                GraphParseState.FIND_NODE_HEADER,
+                GraphParseState.FIND_EDGE_HEADER,
+            ]:
+                pass
+
+            elif state == GraphParseState.PARSE_NODES:
+                lline = (
+                    line.replace("\n", "")
+                    .replace("\t", "")
+                    .replace("\r", "")
+                    .split(" ")
+                )
+                while "" in lline:
+                    lline.remove("")
+                
+
+                node = GraphNode(
+                    {
+                        "id": int(lline[0]),
+                        "name": lline[1],
+                        "period": int(lline[2]),
+                        "capacity": int(lline[3]),
+                        "deadline": int(lline[4]),
+                    }
+                )
+                
+                graph.add_node(node)
+
+            elif state == GraphParseState.PARSE_EDGES:
+                #print(graph.get_nodes())
+                # for nn in graph.get_nodes():
+                #     print(nn)
+                lline = (
+                    line.replace("\n", "")
+                    .replace("\t", "")
+                    .replace("\r", "")
+                    .split(" ")
+                )
+                while "" in lline:
+                    lline.remove("")
+                
+                    
+
+                node_from: GraphNode
+                node_to: GraphNode
+                #print(lline)
+                
+                
+                for nn in graph.get_nodes():
+                    if nn.get_data()['id'] == int(lline[1]):
+                        node_from = nn  
+                    elif nn.get_data()['id'] == int(lline[2]):     
+                        node_to = nn
+                data = {
+                    "period" : line[3],
+                    "capacity" : line[4],
+                    "deadline" : line[5],
+                }
+
+                #edge = GraphEdge(node_from, node_to, lline[2])
+                graph.add_edge(node_from, node_to, data)
+
+        return graph
